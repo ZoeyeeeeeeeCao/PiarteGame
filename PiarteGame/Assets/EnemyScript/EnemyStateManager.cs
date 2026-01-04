ff55f4af508fd0d38f118a75a813e38563aecd89
reasonka <aydin.sartaeva@gmail.com>
2026-01-04 22:15:03 +0800

    merged with ecveryone!!!

--- PiarteGame/Assets/EnemyScript/EnemyStateManager.cs
+++ PiarteGame/Assets/EnemyScript/EnemyStateManager.cs
@@ -58,7 +58,7 @@ public class EnemyStateManager : MonoBehaviour
     {
         if (currentState != null)
         {
-            currentState.OnCollisionEnter(this, collision);
+           // currentState.OnCollisionEnter(this, collision);
         }
     }
 }
